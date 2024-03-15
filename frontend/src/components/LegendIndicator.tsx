0de67ccf1175c4fa375111d5f802f266a11757e2
Yil Verdeja <yilverdeja@gmail.com>
2024-03-15 16:49:22 +0800

    feat: add legend + tailwindcss

---
+++ frontend/src/components/LegendIndicator.tsx
@@ -0,0 +1,11 @@
+import { FaClock } from 'react-icons/fa6';
+const LegendIndicator = () => {
+	return (
+		<div className="flex flex-row gap-4 my-4">
+			<FaClock color="orange" size="1.5rem" />
+			<p>indicates held shift with less than 24 hours response time</p>
+		</div>
+	);
+};
+
+export default LegendIndicator;
---
+++ frontend/src/hooks/useShifts.ts
@@ -0,0 +1,16 @@
+import shifts from '../data/shifts';
+
+export interface Shift {
+	startedAt: string;
+	endedAt: string;
+	status: 'DECLINED' | 'CONFIRMED' | 'PENDING';
+	userId: number;
+	chiName: string;
+	lastName: string;
+	firstName: string;
+	role: 'EN' | 'ST' | 'PWH';
+}
+
+const useShifts = () => ({ data: shifts, isLoading: false, error: null });
+
+export default useShifts;
