275a135e45ca8f065f943622fe4326c66a3ab301
Yil Verdeja <yilverdeja@gmail.com>
2024-03-18 16:36:44 +0800

    feat: sort month and day views

--- frontend/src/components/ShiftsMonthView.tsx
+++ frontend/src/components/ShiftsMonthView.tsx
@@ -19,6 +19,10 @@ const ShiftsMonthView = ({ monthKey }: Props) => {
 		0
 	);
 
+	const sortedDays = Object.keys(shiftsByMonthAndDay[monthKey]).sort(
+		(a, b) => new Date(a).getTime() - new Date(b).getTime()
+	);
+
 	return (
 		<div className="w-full rounded-md overflow-hidden flex-none sm:w-2/3 md:w-1/3 mr-8 md:pb-4 border">
 			<header className="flex flex-row gap-2 bg-gray-200 px-2 py-2">
@@ -36,7 +40,7 @@ const ShiftsMonthView = ({ monthKey }: Props) => {
 			</header>
 
 			<div>
-				{Object.keys(shiftsByMonthAndDay[monthKey]).map((dayKey) => (
+				{sortedDays.map((dayKey) => (
 					<ShiftsDayView
 						key={dayKey}
 						monthKey={monthKey}
--- frontend/src/components/ShiftsView.tsx
+++ frontend/src/components/ShiftsView.tsx
@@ -13,9 +13,13 @@ const ShiftsView = ({ searchText }: Props) => {
 		setShifts();
 	}, [setShifts]);
 
+	const sortedMonths = Object.keys(shiftsByMonthAndDay).sort(
+		(a, b) => new Date(a).getTime() - new Date(b).getTime()
+	);
+
 	return (
 		<div className="flex flex-col sm:flex-row gap-4 flex-no-wrap overflow-y-scroll overflow-x-clip sm:overflow-y-clip sm:overflow-x-scroll items-start h-full my-4">
-			{Object.keys(shiftsByMonthAndDay).map((monthKey) => (
+			{sortedMonths.map((monthKey) => (
 				<ShiftsMonthView key={monthKey} monthKey={monthKey} />
 			))}
 		</div>
