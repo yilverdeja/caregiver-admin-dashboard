0e804e3ebf91c36089ced8ca36516ebb20e05ed3
Yil Verdeja <yilverdeja@gmail.com>
2024-03-19 17:59:32 +0800

    feat: add immer for nested states

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
-// TODO: use immer to update states
-// import { immer } from 'zustand/middleware/immer'
+import { immer } from 'zustand/middleware/immer';
 
 export interface Shift {
 	id: number; // id property to define a shift
@@ -18,41 +17,35 @@ type ShiftIdMap = Record<number, { monthKey: string; dayKey: string }>;
 export type GroupedShifts = Record<string, Record<string, Shift[]>>;
 
 const groupShifts = (shifts: Shift[]) => {
-	const groupByMonthAndDay = shifts
-		.sort(
-			(a, b) =>
-				new Date(a.startedAt).getTime() -
-				new Date(b.startedAt).getTime()
-		)
-		.reduce((acc, shift) => {
-			const startedAtDate = new Date(shift.startedAt);
+	const groupByMonthAndDay = shifts.reduce((acc, shift) => {
+		const startedAtDate = new Date(shift.startedAt);
 
-			// format month-year key (e.g. "November 2023")
-			const monthYearKey = startedAtDate.toLocaleString('en-US', {
-				month: 'long',
-				year: 'numeric',
-			});
+		// format month-year key (e.g. "November 2023")
+		const monthYearKey = startedAtDate.toLocaleString('en-US', {
+			month: 'long',
+			year: 'numeric',
+		});
 
-			// format day-key (e.g. "25" for 25th day of the month)
-			const dayKey = startedAtDate.toLocaleString('en-US', {
-				day: '2-digit',
-			});
+		// format day-key (e.g. "25" for 25th day of the month)
+		const dayKey = startedAtDate.toLocaleString('en-US', {
+			day: '2-digit',
+		});
 
-			// initialize month object if it doesn't exist
-			if (!acc[monthYearKey]) {
-				acc[monthYearKey] = {};
-			}
+		// initialize month object if it doesn't exist
+		if (!acc[monthYearKey]) {
+			acc[monthYearKey] = {};
+		}
 
-			// initialize day array if it doesn't exist
-			if (!acc[monthYearKey][dayKey]) {
-				acc[monthYearKey][dayKey] = [];
-			}
+		// initialize day array if it doesn't exist
+		if (!acc[monthYearKey][dayKey]) {
+			acc[monthYearKey][dayKey] = [];
+		}
 
-			// push the shift to the correct day array
-			acc[monthYearKey][dayKey].push(shift);
+		// push the shift to the correct day array
+		acc[monthYearKey][dayKey].push(shift);
 
-			return acc;
-		}, {} as GroupedShifts);
+		return acc;
+	}, {} as GroupedShifts);
 
 	return groupByMonthAndDay;
 };
@@ -71,91 +64,6 @@ const createShiftIdMap = (shifts: GroupedShifts) => {
 
 	return shiftIdToMonthDayMap;
 };
-
-interface ShiftStore {
-	shiftsByMonthAndDay: GroupedShifts;
-	shiftIdToMonthDayMap: ShiftIdMap;
-	isLoading: boolean;
-	error: string | null;
-	searchTerm: string;
-	setSearchTerm: (term: string) => void;
-	filteredShiftsByMonthAndDay: GroupedShifts;
-	setShifts: (shifts: Shift[]) => void;
-	updateShiftStatus: (
-		shiftId: number,
-		status: 'DECLINED' | 'CONFIRMED' | 'PENDING'
-	) => void;
-}
-
-export const useShiftStore = create<ShiftStore>((set, get) => ({
-	shiftsByMonthAndDay: {},
-	shiftIdToMonthDayMap: {},
-	filteredShiftsByMonthAndDay: {},
-	isLoading: false,
-	error: null,
-	setShifts: (shifts) => {
-		const groupedShifts = groupShifts(shifts);
-		set({
-			shiftsByMonthAndDay: groupedShifts,
-			filteredShiftsByMonthAndDay: groupedShifts,
-			shiftIdToMonthDayMap: createShiftIdMap(groupedShifts),
-		});
-	},
-	searchTerm: '',
-	setSearchTerm: (term) => {
-		set({ searchTerm: term });
-		const filtered = filterGroupedShifts(term, get().shiftsByMonthAndDay);
-		set({ filteredShiftsByMonthAndDay: filtered });
-	},
-	updateShiftStatus: (shiftId, newStatus) => {
-		set((state) => {
-			const idMap = state.shiftIdToMonthDayMap;
-			if (!(shiftId in idMap)) {
-				// return the current state if shiftId is not found
-				return { ...state };
-			}
-
-			const { monthKey, dayKey } = idMap[shiftId];
-
-			// update the status in the groupedShifts
-			const updatedGroupedShifts = {
-				...state.shiftsByMonthAndDay,
-				[monthKey]: {
-					...state.shiftsByMonthAndDay[monthKey],
-					[dayKey]: state.shiftsByMonthAndDay[monthKey][dayKey].map(
-						(shift) =>
-							shift.id === shiftId && shift.status === 'PENDING'
-								? { ...shift, status: newStatus }
-								: shift
-					),
-				},
-			};
-
-			// update the filtered GroupedShifts if they exist
-			const updatedFilteredGroupedShifts =
-				state.filteredShiftsByMonthAndDay && {
-					...state.filteredShiftsByMonthAndDay,
-					[monthKey]: {
-						...state.filteredShiftsByMonthAndDay[monthKey],
-						[dayKey]: state.filteredShiftsByMonthAndDay[monthKey][
-							dayKey
-						].map((shift) =>
-							shift.id === shiftId && shift.status === 'PENDING'
-								? { ...shift, status: newStatus }
-								: shift
-						),
-					},
-				};
-
-			return {
-				shiftsByMonthAndDay: updatedGroupedShifts,
-				filteredShiftsByMonthAndDay:
-					updatedFilteredGroupedShifts ||
-					state.filteredShiftsByMonthAndDay,
-			};
-		});
-	},
-}));
 
 const filterGroupedShifts = (
 	searchTerm: string,
@@ -181,3 +89,82 @@ const filterGroupedShifts = (
 	}
 	return filtered;
 };
+
+type State = {
+	shiftsByMonthAndDay: GroupedShifts;
+	filteredShiftsByMonthAndDay: GroupedShifts;
+	shiftIdToMonthDayMap: ShiftIdMap;
+	isLoading: boolean;
+	error: string | null;
+	searchTerm: string;
+};
+
+type Actions = {
+	setSearchTerm: (term: string) => void;
+	filteredShiftsByMonthAndDay: GroupedShifts;
+	setShifts: (shifts: Shift[]) => void;
+	updateShiftStatus: (
+		shiftId: number,
+		status: 'DECLINED' | 'CONFIRMED' | 'PENDING'
+	) => void;
+};
+
+export const useShiftStore = create<State & Actions>()(
+	immer((set) => ({
+		shiftsByMonthAndDay: {},
+		shiftIdToMonthDayMap: {},
+		filteredShiftsByMonthAndDay: {},
+		isLoading: false,
+		error: null,
+		searchTerm: '',
+		setShifts: (shifts) => {
+			set((state) => {
+				const groupedShifts = groupShifts(shifts);
+				state.shiftsByMonthAndDay = groupedShifts;
+				state.filteredShiftsByMonthAndDay = filterGroupedShifts(
+					state.searchTerm,
+					groupedShifts
+				);
+				state.shiftIdToMonthDayMap = createShiftIdMap(groupedShifts);
+			});
+		},
+		setSearchTerm: (term) => {
+			set((state) => {
+				state.searchTerm = term;
+				state.filteredShiftsByMonthAndDay = filterGroupedShifts(
+					term,
+					state.shiftsByMonthAndDay
+				);
+			});
+		},
+		updateShiftStatus: (shiftId, newStatus) => {
+			set((state) => {
+				const idMap = state.shiftIdToMonthDayMap;
+				if (shiftId in idMap) {
+					const { monthKey, dayKey } = idMap[shiftId];
+					const shifts = state.shiftsByMonthAndDay[monthKey][dayKey];
+					const filteredShifts =
+						state.filteredShiftsByMonthAndDay[monthKey]?.[dayKey] ||
+						[];
+
+					shifts.forEach((shift) => {
+						if (
+							shift.id === shiftId &&
+							shift.status === 'PENDING'
+						) {
+							shift.status = newStatus;
+						}
+					});
+					filteredShifts.forEach((shift) => {
+						if (
+							shift.id === shiftId &&
+							shift.status === 'PENDING'
+						) {
+							shift.status = newStatus;
+						}
+					});
+				}
+			});
+		},
+	}))
+);
